bee0c05f450824f4508b60812cea2b23121e3d4e
sony-level <sonydilane@gmail.com>
2024-04-02 06:53:29 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCUXDi1a-6yV-EjOpxHAa5AnY7YDjetjB4",
+  authDomain: "portofolio-8ce19.firebaseapp.com",
+  projectId: "portofolio-8ce19",
+  storageBucket: "portofolio-8ce19.appspot.com",
+  messagingSenderId: "823148744151",
+  appId: "1:823148744151:web:75ec576497f7c9441f0f92",
+  measurementId: "G-64W0X9BXG3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+console.log("Firebase is successfully initialized!");
+
 particlesJS("particles-js", {
 	"particles": {
 		"number": {
